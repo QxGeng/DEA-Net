d9363e3ee75415154efbc125d8b57f16ba6e5785
unknown <779648726@qq.com>
2021-12-18 12:04:09 +0800

    optimize

--- mmdet/models/necks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .fpn import FPN
-from .bfp import BFP
-from .hrfpn import HRFPN
-from .fpn_coco_transformer import FPNTransformer_COCO
-
-__all__ = ['FPN', 'BFP', 'HRFPN', 'FPNTransformer_COCO']
